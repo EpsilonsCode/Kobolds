e366dc8ecaea3da6f1d567a9605608a7a2c826ff
Salju <61861078+Jusey1@users.noreply.github.com>
2021-11-11 20:03:25 -0500

    Add files via upload

--- src/main/java/net/mcreator/kobolds/init/KoboldsModModels.java
+++ src/main/java/net/mcreator/kobolds/init/KoboldsModModels.java
@@ -1,18 +1,18 @@
 package net.mcreator.kobolds.init;
 
-import net.mcreator.kobolds.client.model.TestModelKobold;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.mcreator.kobolds.client.model.KoboldModel;
+import net.mcreator.kobolds.client.model.KoboldChildModel;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = {Dist.CLIENT})
 public class KoboldsModModels {
 	@SubscribeEvent
 	public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
-
-		event.registerLayerDefinition(ModelKobold.LAYER_LOCATION, TestModelKobold::createBodyLayer);
+		event.registerLayerDefinition(KoboldModel.KOBOLD_MODEL, KoboldModel::createBodyLayer);
+		event.registerLayerDefinition(KoboldChildModel.KOBOLD_CHILD_MODEL, KoboldChildModel::createBodyLayer);
 	}
 }
